390b27b576fe856e0656ea99eff1a599211a388e
artagny <itho.dev11.gny@gmail.com>
2026-06-08 11:34:51 +0700

    refactor: replace direct drizzle connection with a mysql2 promise pool to enforce UTC timezone

--- packages/cbt-server/src/common/db/drizzle.ts
+++ packages/cbt-server/src/common/db/drizzle.ts
@@ -1,8 +1,14 @@
 import { drizzle } from 'drizzle-orm/mysql2';
+import mysql from 'mysql2/promise';
 import { config } from 'dotenv';
 
 config();
 
-export const db = drizzle(process.env.DATABASE_URL ?? (() => { throw new Error('DATABASE_URL is not defined'); })());
+const pool = mysql.createPool({
+    uri: process.env.DATABASE_URL ?? (() => { throw new Error('DATABASE_URL is not defined'); })(),
+    timezone: '+00:00',
+});
+
+export const db = drizzle(pool);
 
 export type DbTransaction = Parameters<Parameters<typeof db.transaction>[0]>[0];
